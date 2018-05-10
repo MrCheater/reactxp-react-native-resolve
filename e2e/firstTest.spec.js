4538e7ab567168e1d266991e649ade1671c8698c
Yuriy Krutilin <y.krutilin@gmail.com>
2018-05-10 17:26:00 +0300

    almost works

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -1,22 +1,8 @@
 const { blacklistLiveReloadUrl, getAppUrl } = require("detox-expo-helpers");
 
 describe("Root testing suite", () => {
-  function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-  const delay = 100000;
   beforeEach(async () => {
-    await timeout(delay);
-    let url = await getAppUrl();
-    device.launchApp({
-      newInstance: true,
-      url,
-      sourceApp: "host.exp.exponent",
-      launchArgs: { EXKernelDisableNuxDefaultsKey: true }
-    });
-    console.log("wait " + delay + " ms");
-    await timeout(delay);
-    await blacklistLiveReloadUrl();
+    await device.reloadReactNative();
   });
 
   it("should have todolist screen", async () => {
--- resolve.build.config.js
+++ resolve.build.config.js
@@ -1,17 +1,17 @@
-import fs from 'fs'
-import path from 'path'
-import nodeExternals from 'webpack-node-externals'
+import fs from "fs";
+import path from "path";
+import nodeExternals from "webpack-node-externals";
 
-import getModulesDirs from 'resolve-scripts/dist/core/get_modules_dirs'
-import getWebpackEnvPlugin from 'resolve-scripts/dist/core/get_webpack_env_plugin'
-import getWebpackResolveAliasPlugin from 'resolve-scripts/dist/core/get_webpack_resolve_alias_plugin'
+import getModulesDirs from "resolve-scripts/dist/core/get_modules_dirs";
+import getWebpackEnvPlugin from "resolve-scripts/dist/core/get_webpack_env_plugin";
+import getWebpackResolveAliasPlugin from "resolve-scripts/dist/core/get_webpack_resolve_alias_plugin";
 
-const host = process.argv[3] || require('my-local-ip')();
+const host = process.argv[3] || require("my-local-ip")();
 
-try{
-  fs.mkdirSync('dist')
+try {
+  fs.mkdirSync("dist");
 } catch (e) {}
-const pathToMain = path.resolve(__dirname, 'client/index.native.js');
+const pathToMain = path.resolve(__dirname, "client/index.native.js");
 
 export default (webpackConfigs, { resolveConfig, deployOptions, env }) => {
   fs.writeFileSync(
@@ -43,24 +43,29 @@ const store = createStore({
   rootPath
 })
 
-RX.App.initialize(true, true)
-RX.UserInterface.setMainView(
+const App = () => (
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <Routes routes={routes} />
     </ConnectedRouter>
   </Provider>
-)
+);
 
-`)
-
+RX.App.initialize(true, true);
+export default App;
+RX.UserInterface.registerRootView("expoToDo", () => App);
+`
+  );
 
   webpackConfigs.push({
-    name: 'Server',
-    entry: ['babel-regenerator-runtime', path.resolve(__dirname, 'client/index.native.js')],
+    name: "Server",
+    entry: [
+      "babel-regenerator-runtime",
+      path.resolve(__dirname, "client/index.native.js")
+    ],
     mode: deployOptions.mode,
-    devtool: 'source-map',
-    target: 'node',
+    devtool: "source-map",
+    target: "node",
     node: {
       __dirname: true,
       __filename: true
@@ -69,10 +74,10 @@ RX.UserInterface.setMainView(
       modules: getModulesDirs()
     },
     output: {
-      path: path.resolve(__dirname, 'dist') ,
-      filename: 'expo.main.js',
-      devtoolModuleFilenameTemplate: '[absolute-resource-path]',
-      devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
+      path: path.resolve(__dirname, "dist"),
+      filename: "expo.main.js",
+      devtoolModuleFilenameTemplate: "[absolute-resource-path]",
+      devtoolFallbackModuleFilenameTemplate: "[absolute-resource-path]?[hash]"
     },
     module: {
       rules: [
@@ -80,10 +85,10 @@ RX.UserInterface.setMainView(
           test: /\.js$/,
           loaders: [
             {
-              loader: 'babel-loader?cacheDirectory=true'
+              loader: "babel-loader?cacheDirectory=true"
             }
           ],
-          exclude: [...getModulesDirs(), path.resolve(__dirname, 'dist')]
+          exclude: [...getModulesDirs(), path.resolve(__dirname, "dist")]
         }
       ]
     },
@@ -91,53 +96,52 @@ RX.UserInterface.setMainView(
       getWebpackEnvPlugin({ resolveConfig, deployOptions, env }),
       getWebpackResolveAliasPlugin({ resolveConfig, deployOptions, env })
     ],
-    externals: getModulesDirs().map(modulesDir => nodeExternals({ modulesDir, whitelist: 'resolve-scripts' }))
-  })
+    externals: getModulesDirs().map(modulesDir =>
+      nodeExternals({ modulesDir, whitelist: "resolve-scripts" })
+    )
+  });
 
-
-
-//   fs.writeFileSync(
-//       pathToMain,
-//       `
-// import React from 'react'
-// import RX from 'reactxp'
-// import createHistory from 'history/createMemoryHistory'
-// import { Provider } from 'react-redux'
-// import { ConnectedRouter } from 'react-router-redux'
-//
-//
-// $resolve = ${JSON.stringify(resolveConfig, null, 2)}
-//
-// const { Routes, createStore } = require('resolve-scripts')
-//
-//
-// const routes = require("${resolveConfig.routes}")
-// const rootPath = "${resolveConfig.rootPath}"
-//
-// const initialState = {}
-//
-// const origin = "${host}:${resolveConfig.port}"
-//
-// const history = createHistory({
-//     basename: rootPath
-// })
-//
-// const store = createStore({
-//   initialState,
-//   history,
-//   origin,
-//   rootPath
-// })
-//
-// RX.App.initialize(true, true)
-// RX.UserInterface.setMainView(
-//   <Provider store={store}>
-//     <ConnectedRouter history={history}>
-//         <Routes routes={routes} />
-//     </ConnectedRouter>
-//   </Provider>
-// )
-//         `.trim()
-//     )
-
-}
+  //   fs.writeFileSync(
+  //       pathToMain,
+  //       `
+  // import React from 'react'
+  // import RX from 'reactxp'
+  // import createHistory from 'history/createMemoryHistory'
+  // import { Provider } from 'react-redux'
+  // import { ConnectedRouter } from 'react-router-redux'
+  //
+  //
+  // $resolve = ${JSON.stringify(resolveConfig, null, 2)}
+  //
+  // const { Routes, createStore } = require('resolve-scripts')
+  //
+  //
+  // const routes = require("${resolveConfig.routes}")
+  // const rootPath = "${resolveConfig.rootPath}"
+  //
+  // const initialState = {}
+  //
+  // const origin = "${host}:${resolveConfig.port}"
+  //
+  // const history = createHistory({
+  //     basename: rootPath
+  // })
+  //
+  // const store = createStore({
+  //   initialState,
+  //   history,
+  //   origin,
+  //   rootPath
+  // })
+  //
+  // RX.App.initialize(true, true)
+  // RX.UserInterface.setMainView(
+  //   <Provider store={store}>
+  //     <ConnectedRouter history={history}>
+  //         <Routes routes={routes} />
+  //     </ConnectedRouter>
+  //   </Provider>
+  // )
+  //         `.trim()
+  //     )
+};
